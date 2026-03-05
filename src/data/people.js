257e65077727d698059247e41e7eb4fa6e151c98
lenovo <ihozomerveille@gmail.com>
2026-03-05 12:36:33 +0200

    added people.js

--- src/data/people.js
+++ src/data/people.js
@@ -1,21 +1,21 @@
 const people = [
   {
     id: 1,
-    name: "Alice Johnson",
+    name: "Merveille Ihozo",
     role: "Software Engineer",
     department: "Engineering",
     avatar: "https://i.pravatar.cc/150?img=1"
   },
   {
     id: 2,
-    name: "David Smith",
+    name: "David Muhire",
     role: "Product Manager",
     department: "Product",
     avatar: "https://i.pravatar.cc/150?img=2"
   },
   {
     id: 3,
-    name: "Sarah Lee",
+    name: "Sarah Uwase",
     role: "UI Designer",
     department: "Design",
     avatar: "https://i.pravatar.cc/150?img=3"
