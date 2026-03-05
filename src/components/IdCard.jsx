e8bfba633f6d3f1b72cc3d40adfc2d6e4a21090e
lenovo <ihozomerveille@gmail.com>
2026-03-05 10:34:17 +0200

    Initial project setup with Vite and Tailwind

---
+++ src/components/IdCard.jsx
@@ -0,0 +1,12 @@
+function IdCard() {
+  return (
+    <div>
+      <h2>ID Card</h2>
+      <p>Name: Mae Jemison</p>
+      <p>Role: Developer</p>
+      <p>Department: IT</p>
+    </div>
+  )
+}
+
+export default IdCard
