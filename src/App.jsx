60f02b88247c29fa3ed8fa67630b0957b1fddbf2
lenovo <ihozomerveille@gmail.com>
2026-03-05 16:01:11 +0200

    about to end

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-10">
       <h1 className="text-3xl font-bold text-center mb-8">
-        ID Card Generator
+        Merveille's ID Card Generator
       </h1>
 
       <CardList />
