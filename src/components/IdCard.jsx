3bb75261c6d7abc2f9af10beb18fc4bb018f6bae
lenovo <ihozomerveille@gmail.com>
2026-03-05 10:57:34 +0200

    added inputs in components

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -1,10 +1,23 @@
-function IdCard() {
+function IdCard({ name, role, department, avatar, id }) {
   return (
-    <div>
-      <h2>ID Card</h2>
-      <p>Name: Mae Jemison</p>
-      <p>Role: Developer</p>
-      <p>Department: IT</p>
+    <div className="bg-white shadow-lg rounded-xl p-6 w-72 text-center border">
+
+      <img
+        src={avatar}
+        alt={name}
+        className="w-24 h-24 rounded-full mx-auto mb-4"
+      />
+
+      <h2 className="text-xl font-bold">{name}</h2>
+
+      <p className="text-gray-600">{role}</p>
+
+      <p className="text-gray-500 text-sm">{department}</p>
+
+      <p className="mt-3 font-semibold text-blue-600">
+        ID: {id}
+      </p>
+
     </div>
   )
 }
