8f7eab9885c01d797e2cc2283411812020cbb9d9
lenovo <ihozomerveille@gmail.com>
2026-03-05 16:02:02 +0200

    Stopping here

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import CardList from "./components/CardList"
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-10">
+    <div className="min-h-screen bg-pink-100 p-10">
       <h1 className="text-3xl font-bold text-center mb-8">
         Merveille's ID Card Generator
       </h1>
